ed75eec79b47827d5713493a516f83a560904018
Yann Pringault <ypringau@student.42.fr>
2015-02-04 12:34:31 +0100

    style(home): search results

--- client/views/home/home.controller.js
+++ client/views/home/home.controller.js
@@ -1,12 +1,48 @@
 'use strict';
 
 angular.module('soundgether')
-  .controller('HomeCtrl', function () {
+  .controller('HomeCtrl', function (soundcloud) {
 
     var vm = this;
 
     angular.extend(vm, {
       name: 'HomeCtrl'
     });
+
+    vm.query = null;
+    vm.results = null;
+    vm.searchTop = false;
+
+    vm.search = function (query) {
+      vm.results = [];
+      setTimeout(function () {
+        soundcloud.search(query).then(function (res) {
+          vm.results = res;
+        })
+      }, 500);
+    };
+
+    vm.getArtwork = function (result, size) {
+      var artwork = result.artwork_url;
+
+      if (artwork === null) {
+        artwork = result.user.avatar_url;
+      }
+
+      return (artwork.replace('large', size));
+    };
+
+    vm.getDuration = function (input) {
+      var duration = moment.duration(input),
+          ret = "";
+      if (duration.hours() > 0) {
+        ret += ('0' + duration.hours()).slice(-2) + ":";
+      }
+      if (duration.minutes() > 0) {
+        ret += ('0' + duration.minutes()).slice(-2) + ":";
+      }
+      ret += ('0' + duration.seconds()).slice(-2);
+      return ret;
+    }
 
   });
